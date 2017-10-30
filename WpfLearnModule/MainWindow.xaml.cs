c296bcf03b1122c36d7268c8da4387969d2b55e4
altukhov <isntc@bitbucket.org>
2017-10-30 22:32:27 +0200

    Add media element

--- WpfLearnModule/MainWindow.xaml.cs
+++ WpfLearnModule/MainWindow.xaml.cs
@@ -24,5 +24,21 @@ namespace WpfLearnModule
         {
             InitializeComponent();
         }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            VideoControl.Source = new Uri(@"F:\Sergej\LearnEnglish\VOA LearningEnglish\Lesson1\MainVideo.mp4");
+            VideoControl.Play();
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            VideoControl.Stop();
+        }
+
+        private void btnPause_Click(object sender, RoutedEventArgs e)
+        {
+            VideoControl.Pause();
+        }
     }
 }
